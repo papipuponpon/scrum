bc42f7235e140fd2f90a9b9c96c5c8396c5e50b2
kudou <h.kudou.sys22@morijyobi.ac.jp>
2023-12-12 15:02:23 +0900

    背景を表示させた #2

--- scripts/main_scene.js
+++ scripts/main_scene.js
@@ -20,17 +20,7 @@ class MainScene extends Phaser.Scene {
 
     preload() {
         // 画像の読み込み(使用する時の名前, パス)
-        this.load.image('sky', 'assets/sky.png');
-        this.load.image('back', 'assets/back.png');
-        this.load.image('ground', 'assets/platform.png');
-        this.load.image('alien1', 'assets/alien1.png');
-        this.load.image('alien2', 'assets/alien2.png');
-        this.load.image('alien3', 'assets/alien3.png');
-        this.load.image('platform', 'assets/platform.png');
-        this.load.image('block', 'assets/block.png');
-        this.load.image('bomb', 'assets/bomb.png');
-        this.load.image('star', 'assets/star.png');
-        this.load.image('coin', 'assets/coin.png');
+        this.load.image('back', 'assets/background.png');
     }
 
 
@@ -38,27 +28,14 @@ class MainScene extends Phaser.Scene {
 
     create() {
         // 単体画像をシーンに追加(X座標,Y座標,画像名)
-        this.add.image(400, 300, 'sky');
-        this.add.image(200, 200, 'star');
-        this.player = this.add.image(500, 350, 'alien1');
-        // プレイヤーの移動方向フラグを設定　１：右向き　−１：左向き
-        this.player_direction = 1;
+        this.add.image(400, 300, 'back');
     }
 
 
     // 毎フレーム実行される繰り返し処理
 
     update() {
-        if (this.player.x >= D_WIDTH - 100) this.player_direction = -1;
-        if (this.player.x <= 100) this.player_direction = 1;
-
-
-        // プレイヤーの移動
-        if (this.player_direction == 1) {
-            this.player.x += 5;// 横方向へ移動を設定
-        } else {
-            this.player.x -= 5;// 横方向へ移動を設定
-        }
+    
     }
 
 }
